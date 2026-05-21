b4984ab45035740bb771bb4fb66ac44c05a44033
MinTheinKhant244 <mintheinkhant2020@gmail.com>
2026-05-21 19:22:16 +0630

    test_frontend

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -1,14 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
-import userReducer from "../features/user/userSlice";
-import productReducer from "../features/products/productSlice";
-import authReducer from "../features/auth/authSlice";
-import cartReducer from "../features/cart/cartSlice";
+// import userReducer from "../features/user/userSlice";
+// import productReducer from "../features/products/productSlice";
+// import authReducer from "../features/auth/authSlice";
+// import cartReducer from "../features/cart/cartSlice";
 const store=configureStore({
     reducer:{
-        user:userReducer,
-        product:productReducer,
-        auth:authReducer,
-        cart:cartReducer
+        // user:userReducer,
+        // product:productReducer,
+        // auth:authReducer,
+        // cart:cartReducer
     }
 })
 export default store
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import 'chart.js'
+// import 'chart.js'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
-import { DataTable } from 'simple-datatables'
-import './assets/css/styles.css'
+// import { DataTable } from 'simple-datatables'
+// import './assets/css/styles.css'
 import { BrowserRouter } from 'react-router'
 import { Provider } from 'react-redux'
 import store from './app/store.js'
@@ -13,13 +13,15 @@ import store from './app/store.js'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     
-    <BrowserRouter>
+    {/* <BrowserRouter>
 
       <Provider store={store}>
           <App />
       </Provider>
     
-    </BrowserRouter>
+    </BrowserRouter> */}
+
+    <App></App>
    
   </React.StrictMode>,
 )
