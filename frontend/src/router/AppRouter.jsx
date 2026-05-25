e9973fa7a82f39e408770001abe1988fd8bbd1ca
MinTheinKhant244 <mintheinkhant2020@gmail.com>
2026-05-25 15:05:55 +0630

    t3

--- frontend/src/router/AppRouter.jsx
+++ frontend/src/router/AppRouter.jsx
@@ -1,5 +1,6 @@
 import { Routes, Route } from "react-router-dom"
 
+import { Navigate } from "react-router-dom"
 import Login from "../pages/Login"
 import Dashboard from "../pages/admin/Dashboard"
 import CashierHome from "../pages/cashier/CashierHome.jsx"
@@ -8,6 +9,9 @@ import ProtectedRoute from "../components/ProtectedRoute"
 function AppRouter() {
   return (
     <Routes>
+
+       {/* redirect root */}
+      <Route path="/" element={<Navigate to="/login" />} />
 
       <Route path="/login" element={<Login />} />
 
